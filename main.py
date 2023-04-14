a0779d63352202435438cd9aaa4e44c94c5f98c8
thomasdouilly <119968854+thomasdouilly@users.noreply.github.com>
2023-04-14 23:05:35 +0200

    Layout rework

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ import pandas as pd
 data = pd.read_excel('./data/data.xlsx', index_col = "N° auto", true_values = ['OUI'], false_values = ['NON'])
 
 data = data[['nom', 'région administrative', 'commune', 'unité urbaine', 'séances', 'entrées 2020', 'entrées 2021', 'multiplexe']]
+data['multiplexe'].replace(False, 'Miniplex', inplace = True)
+data['multiplexe'].replace(True, 'Multiplex', inplace = True)
 
 regions_list = data.groupby("région administrative").max().index.to_list()
 
-brush = alt.selection_interval()
 
+brush = alt.selection_interval()
 
 def main_plot(year = 2021, regions = regions_list):
     
@@ -27,17 +29,18 @@ def main_plot(year = 2021, regions = regions_list):
             
     
     main_plot = alt.Chart(data).mark_point().encode(
-        x = alt.X('séances', scale = alt.Scale(domain = (0, 25000))),
-        y = alt.Y(y_plot, scale = alt.Scale(domain = (0, 600000))), 
+        x = alt.X('séances', scale = alt.Scale(domain = (0, 40000))),
+        y = alt.Y(y_plot, scale = alt.Scale(domain = (0, 1500000))), 
         tooltip = ["nom", "région administrative"]
-    ).transform_filter(alt.FieldOneOfPredicate(field='région administrative', oneOf = regions))
+    ).transform_filter(alt.FieldOneOfPredicate(field='région administrative', oneOf = regions)).add_selection()
     
     
-    regression_line = main_plot.transform_regression('séances', y_plot, extent = (0, 25000)).encode(color = alt.value("#FF0000")).mark_line()
-    mark_plot = main_plot.encode(color = alt.Color('multiplexe', legend = alt.Legend(title = '', orient='bottom', direction='horizontal', titleAnchor='middle', values = ['Miniplex', 'Multiplex']))).mark_point()
+    regression_line = main_plot.transform_regression('séances', y_plot, extent = (0, 40000)).encode(color = alt.value("#FF0000")).mark_line()
+    mark_plot = main_plot.encode(color = alt.condition(brush, 'multiplexe', alt.value('grey'))).mark_point()
     text = main_plot.mark_text(baseline="middle", dy = 12, fontSize = 10).encode(text="nom").transform_filter(alt.FieldGTPredicate(alt.datum.séances, gt = 1500))
-
-    final_plot = alt.layer(mark_plot + text, regression_line).properties(width=1500, height=1000)
+    selection_line = main_plot.transform_filter(brush)
+    
+    final_plot = alt.layer(mark_plot + text, regression_line)
 
     regression_params = alt_transform.extract_data(regression_line)
     initial_point = regression_params.iloc[0].to_list()
@@ -45,47 +48,78 @@ def main_plot(year = 2021, regions = regions_list):
     
     ratio = (final_point[1] - initial_point[1]) / (final_point[0] - initial_point[0])
     ratio_france = ((data[y_plot] + pad).sum()) / data['séances'].sum()
-        
+    
     return final_plot, ratio, ratio_france
 
 
-st.set_page_config(layout="wide")
-st.title('How to link number of showings and number of ticket solds in French cinemas ?')
+st.set_page_config(page_title = 'Cinema Analysis')
+
+st.markdown("""
+        <style>
+              
+        </style>
+        """, unsafe_allow_html=True)
 
 st.markdown("""
 <style>
 .begin-font {
-    font-size:16px;
+    font-size:14px;
 }
 .ratio-font {
-    font-size:32px !important;
+    font-size:20px !important;
     text-align: center;
 }
 .center-font {
-    font-size:16px;
+    font-size:14px;
     text-align: center;
+}
+.title {
+    font-size:32px
+}
+.block-container {
+    padding-top: 0.5rem;
+    padding-bottom: 0rem;
+    padding-left: 3rem;
+    padding-right: 3rem;
+}
+.reportview-container .sidebar-content {
+    padding-top: 10rem;
+}
+span[data-baseweb="tag"] {
+  background-color: blue !important;
+}
+.st-af {
+    font-size:12px;
+}
+.st-dw {
+    max-width: 400px;
+}
+.css-1544g2n{
+    padding:2rem 0.5rem 0.5rem;
 }
 </style>
 """, unsafe_allow_html=True)
 
+st.markdown('<p class = title><b>How to link number of showings and number of ticket solds in French cinemas ?</b></p>', unsafe_allow_html = True)
 
-year = st.slider('Year', 2020, 2021, 2020, format = None)
 
- 
-regions = st.multiselect("Please select the French 'Régions' to be considered in the graph :", regions_list, regions_list)
+with st.sidebar:
+    year = st.slider('Year :', 2020, 2021, 2021, format = None)
+    cinema_type = st.multiselect("Category of cinemas :", ['Multiplex', 'Miniplex'], ['Multiplex', 'Miniplex'])
+    regions = st.multiselect("Please select the French 'Régions' to be considered in the graph :", regions_list, regions_list)
 
 plot, ratio_selec, ratio_fr = main_plot(year, regions)
-plot = plot.interactive()
+plot = plot.add_selection(brush).properties(width=700, height=600)
 
-st.markdown('<p class = begin-font> In ' + str(year) + ', an average of</p>', unsafe_allow_html=True)
+st.markdown('<p class = begin-font> In ' + str(year) + ', an average of :</p>', unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 
-col1.markdown("<p class = ratio-font>" + str(round(ratio_selec, 1)) + "</p>", unsafe_allow_html=True)
+col1.markdown("<p class = ratio-font><b>" + str(round(ratio_selec, 1)) + "</b></p>", unsafe_allow_html=True)
 col1.markdown('<p class = center-font>tickets were sold for each<br>showing<br>*** SELECTION ONLY ***</p>', unsafe_allow_html=True)
 
 
-col2.markdown("<p class = ratio-font>" + str(round(ratio_fr, 1)) + "</p>", unsafe_allow_html=True)
+col2.markdown("<p class = ratio-font><b>" + str(round(ratio_fr, 1)) + "</b></p>", unsafe_allow_html=True)
 col2.markdown('<p class = center-font>tickets were sold for each<br>showing<br>*** WHOLE FRANCE ***</p>', unsafe_allow_html=True)
 
 st.altair_chart(plot)
