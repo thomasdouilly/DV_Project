6cedaf0e5050de5f910ffda8b88bdcfafad06097
thomasdouilly <119968854+thomasdouilly@users.noreply.github.com>
2023-04-15 16:52:31 +0200

    Version 1.0

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ data['multiplexe'].replace(True, 'Multiplex', inplace = True)
 
 regions_list = data.groupby("région administrative").max().index.to_list()
 
-
-brush = alt.selection_interval()
-
 def main_plot(year = 2021, regions = regions_list, type = ['Miniplex', 'Multiplex']):
     
     if year == 2021:
@@ -28,19 +25,18 @@ def main_plot(year = 2021, regions = regions_list, type = ['Miniplex', 'Multiple
         pad = 2732.255890096887
             
     
-    main_plot = alt.Chart(data).mark_point().encode(
-        x = alt.X('séances', scale = alt.Scale(domain = (0, 40000))),
-        y = alt.Y(y_plot, scale = alt.Scale(domain = (0, 1500000))), 
-        tooltip = ["nom", "région administrative"]
+    main_plot = alt.Chart(data, title = 'French cinemas according to number of showings and tickets sold in ' + str(year)).mark_point().encode(
+        x = alt.X('séances', scale = alt.Scale(domain = (0, 40000)), title = 'Number of showings'),
+        y = alt.Y(y_plot, scale = alt.Scale(domain = (0, 1500000)), title = 'Number of tickets sold in ' + str(year)), 
+        tooltip = [alt.Tooltip("nom", title = 'Cinema name'), alt.Tooltip('commune', title = 'City'), alt.Tooltip("région administrative", title = 'French region'), alt.Tooltip("séances", title = "Number of showings"), alt.Tooltip(y_plot, title = "Ticket solds in " + str(year))]
     ).transform_filter(alt.FieldOneOfPredicate(field='région administrative', oneOf = regions)).transform_filter(alt.FieldOneOfPredicate(field='multiplexe', oneOf = type)).add_selection()
     
     
     regression_line = main_plot.transform_regression('séances', y_plot, extent = (0, 40000)).encode(color = alt.value("#FF0000")).mark_line()
-    mark_plot = main_plot.encode(color = alt.condition(brush, 'multiplexe', alt.value('grey'))).mark_point()
-    text = main_plot.mark_text(baseline="middle", dy = 12, fontSize = 10).encode(text="nom").transform_filter(alt.FieldGTPredicate(alt.datum.séances, gt = 1500))
-    selection_line = main_plot.transform_filter(brush)
+    mark_plot = main_plot.encode(color = alt.Color('multiplexe', legend = alt.Legend(title = "Cinema category", orient = "bottom"))).mark_point()
+    text = main_plot.mark_text(baseline="middle", dy = 12, fontSize = 9).encode(text="nom").transform_filter(alt.FieldGTPredicate(alt.datum.séances, gt = 1500)) 
     
-    final_plot = alt.layer(mark_plot + text, regression_line).configure_legend(orient='bottom')
+    final_plot = alt.layer(mark_plot + text, regression_line)
 
     regression_params = alt_transform.extract_data(regression_line)
     initial_point = regression_params.iloc[0].to_list()
@@ -74,7 +70,7 @@ st.markdown("""
     text-align: center;
 }
 .title {
-    font-size:32px
+    font-size:40px
 }
 .block-container {
     padding-top: 0.5rem;
@@ -94,22 +90,29 @@ span[data-baseweb="tag"] {
 .st-dw {
     max-width: 400px;
 }
+.st-be {
+    padding-top:0px;
+}
 .css-1544g2n{
     padding:2rem 0.5rem 0.5rem;
+}
+.css-10y5sf6 {
+    font-size:0px
 }
 </style>
 """, unsafe_allow_html=True)
 
-st.markdown('<p class = title><b>How to link number of showings and number of ticket solds in French cinemas ?</b></p>', unsafe_allow_html = True)
+st.markdown('<p class = title><b>Increasing the number of showings is often profitable for a French cinema</b></p>', unsafe_allow_html = True)
 
 
 with st.sidebar:
+    st.sidebar.title('Graph personnalization')
     year = st.slider('Year :', 2020, 2021, 2021, format = None)
     cinema_type = st.multiselect("Category of cinemas :", ['Multiplex', 'Miniplex'], ['Multiplex', 'Miniplex'])
-    regions = st.multiselect("Please select the French 'Régions' to be considered in the graph :", regions_list, regions_list)
+    regions = st.multiselect("French 'Régions' :", regions_list, regions_list)
 
 plot, ratio_selec, ratio_fr = main_plot(year, regions, cinema_type)
-plot = plot.add_selection(brush).properties(width=700, height=500)
+plot = plot.properties(width=600, height=425)
 
 tab1, tab2 = st.tabs(["Summary", "Data"])
 
@@ -119,12 +122,12 @@ with tab1:
     col1, col2 = st.columns(2)
 
     col1.markdown("<p class = ratio-font><b>" + str(round(ratio_selec, 1)) + "</b></p>", unsafe_allow_html=True)
-    col1.markdown('<p class = center-font>tickets were sold for each<br>showing<br>*** SELECTION ONLY ***</p>', unsafe_allow_html=True)
+    col1.markdown('<p class = center-font>tickets were sold for each<br>showing<br>--- FILTERED CINEMAS ---</p>', unsafe_allow_html=True)
 
 
     col2.markdown("<p class = ratio-font><b>" + str(round(ratio_fr, 1)) + "</b></p>", unsafe_allow_html=True)
-    col2.markdown('<p class = center-font>tickets were sold for each<br>showing<br>*** WHOLE FRANCE ***</p>', unsafe_allow_html=True)
+    col2.markdown('<p class = center-font>tickets were sold for each<br>showing<br>--- ALL CINEMAS ---</p>', unsafe_allow_html=True)
 
 with tab2:
-    st.altair_chart(plot)
+    st.altair_chart(plot, theme = None)
 
