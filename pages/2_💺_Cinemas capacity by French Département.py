37da27964bcf00e50e5e2ca12cff390a959e7923
Martin Ponchon <martinponchon@MBP-de-Martin.lan>
2023-04-16 13:26:17 +0200

    Add comments and clean code

--- pages/2_💺_Cinemas capacity by French Département.py
+++ pages/2_💺_Cinemas capacity by French Département.py
@@ -9,55 +9,55 @@ import numpy as np
 
 st.set_page_config(page_title="French cinema analysis", page_icon="🎥", layout="wide")
 
+st.title("Dashboard")
+
 
 @st.cache_data
 def get_data():
+    # Get values and geographical data
     value_df = pd.read_excel("pages/data/data_martin.xlsx")
     geo_df = geopandas.read_file('pages/data/departements_martin.geojson')
     df = geo_df.merge(value_df, on='code', how='left').set_index('code')
 
-    #countries_df = geopandas.read_file("pages/data/eu_countries.geojson")
-    #countries = ["France", "Spain", "Belgium", "Italy", "Luxembourg", "Switzerland", "Germany", "Portugal", "Austria", "Netherlands", "Croatia", 'Czech Republic', "Slovenia", 'Andorra','Liechtenstein', 'United Kingdom', "Hungary", "Poland", "Slovakia", 'Bosnia and Herzegovina', "Montenegro", 'Republic of Serbia']
-    #eu_df = countries_df[countries_df.ADMIN.isin(countries)] #.set_index("ADMIN")
+    # Get european countries for background 
     eu_df = geopandas.read_file("pages/data/eu_countries.geojson")
     return df, eu_df
-
-st.title("Dashboard")
 
 
 df, eu_df = get_data()
 
-max = 150
-min = 0
-values = st.slider("Select value range:", min_value=min, value=(min, max), max_value=max)
-
+# Slider to select the range of values to display
+values = st.slider("Number of tickets sold per seat - Select value range:", min_value=0, value=(0, 150), max_value=150)
 min_val, max_val = values
 map_df = df.loc[(df.value >= min_val) & (df.value <= max_val)]
 
-fig = px.choropleth_mapbox(eu_df, geojson=eu_df.geometry, locations=eu_df.index, color=np.zeros_like(eu_df.index),
+# Europe countries as background for the figure
+fig = px.choropleth_mapbox(eu_df, geojson=eu_df.geometry, locations=eu_df.index, color=40*np.ones_like(eu_df.index),
                         color_continuous_scale="Viridis",
-                        center={'lat': 46.5, 'lon': 2},
+                        range_color=(0, 150),
                         mapbox_style='white-bg',
-                        range_color=(25, 110),
-                        zoom=4,
+                        center={'lat': 46.5, 'lon': 2},
+                        zoom=4.1,
                         opacity=0.1,
                         hover_name='ADMIN') #.update(layout_showlegend=False)
 
+# French departments
 fig2 = px.choropleth_mapbox(map_df, geojson=map_df.geometry, locations=map_df.index, color=map_df.value,
                         color_continuous_scale="Viridis",
-                        range_color=(25, 110),
+                        range_color=(0, 150),
                         mapbox_style='white-bg',
-                        zoom=4,
                         center={'lat': 46.5, 'lon': 2},
-                        labels={'value':'Number'},
-                        #hover_data={'code':False},
-                        hover_name='nom', 
-                        opacity=1)
+                        zoom=4.1,
+                        opacity=1,
+                        labels={'value':'Tickets sold per seat'},
+                        hover_name='nom')
 
+# Superpose maps
 fig.add_trace(fig2.data[0])
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, coloraxis_colorbar=dict(title="N", orientation='h', y=1.01, len=1.1))
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, coloraxis_colorbar=dict(title="", orientation='h', y=1, len=1.055, thickness=40))
 fig.update_geos(fitbounds="locations", visible=True)
 
+# Display chart
 st.plotly_chart(fig, use_container_width=True)
 
 
