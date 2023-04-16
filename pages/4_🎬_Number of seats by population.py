5fe8f7946434862b5bc69ac04bc668dac834ed47
thomasdouilly <119968854+thomasdouilly@users.noreply.github.com>
2023-04-16 19:13:34 +0200

    Updated emojis

--- pages/4_🎬_Number of seats by population.py
+++ pages/4_🎬_Number of seats by population.py
@@ -57,7 +57,7 @@ mean_value = int(df['Number of persons per seats'].mean())
 mean_line = alt.Chart(pd.DataFrame({'mean_value': [mean_value]})).mark_rule(color='lightblue').encode(
     y=alt.Y('mean_value:Q', axis=alt.Axis(title='Number of persons per seats', titleColor='green'))
 )
-mean_label = alt.Chart(pd.DataFrame({'mean_value': [mean_value]})).mark_text(color='lightblue', dx=-395, dy=0, align='left').encode(
+mean_label = alt.Chart(pd.DataFrame({'mean_value': [mean_value]})).mark_text(color='lightblue', dx=-405, dy=0, align='left').encode(
     y=alt.Y('mean_value:Q'),
     text=alt.Text('mean_value:Q')
 )
